6d99b337a43c7d7d72ce4b360a4e1babbd4f471c
Wachira-joy <wachiraglorywairimu@gmail.com>
2024-04-04 18:04:22 +0300

    added the last question

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 //Write a function that accepts an array of strings and console.logs each element using a for loop.
-let arr = (["Hello", "World", "This", "Is", "My", "String"]); 
+let arr = (["Hello", "Ada", "What", "is", "that", "shirt"]); 
 
-for (let element of arr) { 
-    console. log(element. toString()) } 
-     let myFunction = (element) => element
+for (let x of arr) { 
+    console. log(x. toString()) } 
+     let myFunction = (x) => x
 
 //Write a JavaScript function that takes a string as input and reverses it using a while loop. The function should return the reversed string. 
 
      function reverseString(str) {
         let reversed = '';
-        let i = str.length - 1;
-        while (i >= 0) {
-            reversed += str[i];
-            i--;
+        let y = str.length - 1;
+        while (y >= 0) {
+            reversed += str[y];
+            y--;
         }
         return reversed;
     }
     
     // Example usage
-    let originalString = "Hello, World!";
-    let reversedString = reverseString(originalString);
+    let firstString = "Glory, Joy!";
+    let reversedString = reverseString(firstString);
     console.log(reversedString); 
 
 //Write a function that takes in a an array of strings and use a continue statement when the item is at the second index:  let fruits= ['apple','plum','banana','strawberries','kiwi']
-    function processFruits(fruits) {
-        for (let i = 0; i < fruits.length; i++) {
-            if (i === 2) {
-                continue; // Skips the current iteration when i is at the second index
+    function takeFruits(fruits) {
+        for (let a = 0; a < fruits.length; a++) {
+            if (a === 2) {
+                continue; 
             }
-            console.log(fruits[i]);
+            console.log(fruits[a]);
         }
     }
     
-    // Example usage
+   
     let fruits = ['apple', 'plum', 'banana', 'strawberries', 'kiwi'];
-    processFruits(fruits);
+    takeFruits(fruits);
 
 
 //Write a program that takes in the following array and use a while loop to iterate and break when the item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9];
-let arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let i = 0;
+let arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let f = 0;
 
-while (i < arrNum.length) {
-    if (i === 4) {
-        break; // Exit the loop when the item is at the fourth index
+while (f < arrNumbers.length) {
+    if (f === 4) {
+        break; 
     }
-    console.log(arrNum[i]);
-    i++;
+    console.log(arrNumbers[f]);
+    f++;
 }
 
 //Write a program that takes in an array of numbers and consoles the first four items multiplied by itself  and the last two added by 10. Return the array with the new values
